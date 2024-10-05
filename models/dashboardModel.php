b59957ae7f3f451c54229fccbd5e6647e145d526
jose castro <josecastro79@gmail.com>
2024-10-05 13:43:49 -0400

    contador dashboard

--- models/dashboardModel.php
+++ models/dashboardModel.php
@@ -10,7 +10,46 @@ class dashboardModel extends ModeloBase
         parent::__construct();
     }
 
-    /*------------Metodo para listar Especies--------*/
+    /*------------Metodo para contar citas-------*/
+    public function numeroCitas()
+    {
+        $db = new ModeloBase();
+        $query = "SELECT count(*) as numeroCitas from citas;";
+        $resultado = $db->obtenerTodos($query);
+        return $resultado;
+    }
+
+/*------------Metodo para contar consultas-------*/
+public function numeroConsultas()
+{
+    $db = new ModeloBase();
+    $query = "SELECT count(*) as numeroConsultas from consultas;";
+    $resultado = $db->obtenerTodos($query);
+    return $resultado;
+}
+ 
+/*------------Metodo para contar pacientes atendidos-------*/
+public function pacientesAtendidos()
+{
+    $db = new ModeloBase();
+    $query = "SELECT count(*) as numeroPacientesAt from citas where estatus=0;";
+    $resultado = $db->obtenerTodos($query);
+    return $resultado;
+}
+
+/*------------Metodo pacientes atendidos hoy-------*/
+public function pacientesAtendidosGeneral()
+{
+    $db = new ModeloBase();
+    $query = "SELECT 
+    (SELECT COUNT(*) FROM citas WHERE estatus = 0) AS total_citas_estatus_0,
+    (SELECT COUNT(*) FROM consultas) AS total_consultas,
+    (SELECT COUNT(*) FROM citas WHERE estatus = 0) + (SELECT COUNT(*) FROM consultas) AS total_general
+FROM dual;";
+    $resultado = $db->obtenerTodos($query);
+    return $resultado;
+}
+/*------------Metodo para listar Especies--------*/
     public function totalEquivUSD()
     {
         $db = new ModeloBase();
